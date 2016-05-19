7bd83d54420f43929d5361888ddc6cba5a02ec82
Danila <O4epegb@mail.ru>
2016-05-19 23:32:58 +0400

    init

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const fs = require('fs');
 const Hapi = require('hapi');
+const Ivona = require('ivona-node');
+const keys = require('./secret');
+const path = require('path')
+
+var Readable = require('stream').Readable;
 
 const port = process.env.PORT || 3000
 
@@ -8,6 +14,20 @@ const server = new Hapi.Server();
 server.connection({
     port: port
 });
+console.log(keys);
+var ivona = new Ivona({
+    accessKey: keys.accessKey,
+    secretKey: keys.secretKey
+});
+
+// ivona.listVoices()
+//     .on('complete', function(voices) {
+//         console.log(voices);
+//     });
+
+// ivona.createVoice(text, config)
+// [string] text - the text to be spoken
+// [object] config (optional) - override Ivona request via 'body' value
 
 server.register(require('inert'), (err) => {
     if (err) {
@@ -16,18 +36,56 @@ server.register(require('inert'), (err) => {
     server.route({
         method: 'GET',
         path: '/{path*}',
-        // handler: {
-        //     file: './public/index.html'
-        // }
-        // handler: function (request, reply) {
-        //     reply.file('./public/index.html', './public/common.js');
-        // }
         handler: {
             directory: {
                 path: 'public'
             }
         }
     });
+});
+
+server.route({
+    method: 'POST',
+    path: '/test',
+    handler: function(request, reply) {
+        // console.log(request.payload)
+        // var test = ivona.createVoice('Привет! Я Максим.', {
+        //         body: {
+        //             voice: {
+        //                 name: 'Maxim',
+        //                 language: 'ru-RU',
+        //                 gender: 'Male'
+        //             }
+        //         }
+        //     })
+        // .pipe(fs.createWriteStream('text.mp3'));
+
+        // console.log(test);
+        // var filepath = path.join(__dirname, 'text.mp3');
+        // console.log(filepath);
+        // var readStream = fs.createReadStream(filepath)
+        // reply(test).type('audio/mp3');
+
+        var test = ivona.createVoice(request.payload.text, {
+                body: {
+                    voice: {
+                        name: 'Maxim',
+                        language: 'ru-RU',
+                        gender: 'Male'
+                    }
+                }
+            })
+            // .on('response', function(response) {
+            //     // console.log(response);
+            //     const test = reply(response);
+            //     test.type('audio/mp3');
+            //     test.variety = 'buffer';
+            // })
+            //     .pipe(fs.createWriteStream('text.mp3'));
+
+            reply(new Readable().wrap(test));
+        // res.setHeader("Content-Type", "audio/mpeg");
+    }
 });
 
 server.start((err) => {
--- public/common.js
+++ public/common.js
@@ -1,3 +1,40 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+
+function process(Data) {
+    source = context.createBufferSource(); // Create Sound Source
+    context.decodeAudioData(Data, function(buffer) {
+        source.buffer = buffer;
+        source.connect(context.destination);
+        source.start(context.currentTime);
+    })
+}
+
+function loadSound(text) {
+  var request = new XMLHttpRequest();
+  request.open("POST", "/test", true);
+  request.responseType = "arraybuffer";
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+  var body = 'text=' + encodeURIComponent(text);
+
+  request.send(body);
+  request.onload = function() {
+      var data = request.response;
+      console.log(data);
+      process(data);
+  };
+};
+
 $('button').click(function() {
-  console.log('hello')
-})
+    console.log('hello, we are testing hapi and ivona');
+    var text = $('textarea').val() || 'Привет! Меня зовут Максим.';
+    loadSound(text)
+    // $.post("/test", {
+    //     name: "John",
+    //     time: "2pm"
+    // }).then(function(data) {
+    //     // $(".result").html(data);
+    //     console.log(data);
+    //     process(data)
+    // });
+});
